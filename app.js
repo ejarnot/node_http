aed8aea44ab871e7266339d22c93050ea92c895c
Mia Jarnot <ejarnot42@outlook.com>
2022-04-19 18:13:02 -0700

    finished exercise

--- app.js
+++ app.js
@@ -6,3 +6,28 @@ console.log("EXERCISE 1:\n==========\n");
 const http = require("http");
 
 // Finish setting up the server
+
+http
+  .createServer(function (req, res) {
+    const { method, url } = req
+    const chunks = []  
+    req.on("data", function(chunk){
+          chunks.push(chunk)
+      })
+    req.on("end", function(){
+        const body = JSON.parse(Buffer.concat(chunks).toString())
+    })
+
+    if(url == "/"){
+        res.statusCode = 200;
+        res.write("<h1>DON'T PANIC</h1>")
+    }else if(url == "/about"){
+        res.statusCode = 200;
+        res.write("<h1>Mia lives in Portland, OR</h1>")
+    }else if(url == "/echo"){
+        res.statusCode = 200;
+        res.write({method, url, body})
+    }
+    res.end();
+  })
+  .listen(3000)
